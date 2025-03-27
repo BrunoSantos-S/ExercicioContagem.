708b30a38cdd56cab485169eb67a64a955722935
BrunoSantos-S <7225114660@ulife.com.br>
2025-03-27 08:44:33 -0300

    Delete ExercicioContagem directory

--- ExercicioContagem/src/Contagem.java
+++
@@ -1,7 +0,0 @@
-public class Contagem {
-    public static void main(String[] args) {
-        // Loop de 1 a 10, imprimindo um número por linha
-        for (int i = 1; i <= 10; i++)
-            System.out.println(i);
-    }
-}
