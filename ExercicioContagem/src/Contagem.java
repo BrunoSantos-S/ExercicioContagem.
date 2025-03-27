26aae7819c3c44e32c392719dfe2967853b5faa7
BrunoSantos-S <7225114660@ulife.com.br>
2025-03-27 08:28:05 -0300

    Add files via upload

---
+++ ExercicioContagem/src/Contagem.java
@@ -0,0 +1,7 @@
+public class Contagem {
+    public static void main(String[] args) {
+        // Loop de 1 a 10, imprimindo um número por linha
+        for (int i = 1; i <= 10; i++)
+            System.out.println(i);
+    }
+}
